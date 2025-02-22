12c7be84b1369c2bff4427e40134e8a3dcd5ce51
Lily Mosisa <lilymosisa@Lilys-MacBook-Air.local>
2025-02-22 15:09:04 -0500

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';  // Import React to use JSX
+import React, {useState} from 'react';  // Import React to use JSX
 function StartButton() {
-  coonst [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
   function handleClick() {
     setShowForm(!showForm);
   }
-<Form/>
   return (
     <div> 
     <button onClick={handleClick}>
@@ -16,29 +15,29 @@ function StartButton() {
 }
 function Form(){
   return(
-    <form action="" method="get" class="form-rabbit">
+    <form action="" method="get" className="form-rabbit">
       
-    <div class="form-rabbit">
+    <div className="form-rabbit">
      <label for="goal">Enter your goal: </label>
      <input type="text" name="goal" id="goal" required />
     </div>
     
-   <div class="form-rabbit">
+   <div className="form-rabbit">
       <label for="deadline">Enter your deadline: </label>
       <input type="time" name="deadline" id="deadline" required />
    </div>
   
-   <div class="form-rabbit">
+   <div className="form-rabbit">
       <label for="number">Enter a phone #: 	</label>
       <input type= "tel" name="pnumber" id="pnumber" required />
    </div>
 
-    <div class="form-rabbit">
+    <div className="form-rabbit">
      <label for="mess">Enter your message: </label>
      <input type="text" name="mess" id="mess" required />
     </div>
     
-    <div class="form-rabbit">
+    <div className="form-rabbit">
       <input type="submit" value="Submit!" />
     </div>
     </form>
@@ -49,6 +48,7 @@ function App() {
   return (
     <div>
       <h1>Hello, React!</h1>
+      <StartButton/>
     </div>
   );
 }
