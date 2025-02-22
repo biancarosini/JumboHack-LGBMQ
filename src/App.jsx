2b595d5035859a2e8ab0d510c1c0f59d7dac37a0
biancarosini <biancarosini@icloud.com>
2025-02-22 15:49:22 -0500

    committing to pull changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';  // Import React to use JSX
 
 function App() {
-  return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
+  return <h1>Hello, React!</h1>;
 }
 
 export default App; 
